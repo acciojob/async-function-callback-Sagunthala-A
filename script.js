35fcde9bc78cacf74d85c2ad18e3ebc024bf1aec
Sagunthala A <117196512+Sagunthala-A@users.noreply.github.com>
2024-08-18 08:00:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 const url = "https://jsonplaceholder.typicode.com/posts/1";
 
-//your JS code here. If required.
+let btn = document.getElementById("btn");
+let div = document.getElementById("output");
+
+btn.addEventListener("click", () => {
+  fetching(callbc);
+});
+
+function callbc(data) {
+  div.innerText = data.title;
+}
+
+async function fetching(callback) {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+  const data = await res.json();
+  callback(data);
+}
